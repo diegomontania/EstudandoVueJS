8eb64c15dff8248f3da71dd2e694f19eae56f538
diego montania <diegoasmad1@gmail.com>
2020-11-24 17:54:38 -0300

    Adicionando imagens

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,47 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+  <div>
+    <h1>{{ titulo }}</h1>
+
+    <!-- lista de imagens -->
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+
+      <!-- faz a iteracao (loop) entre as fotos-->
+      <!-- <li v-for="foto of fotos"> 
+        <img v-bind:src="foto.url" v-bind:alt="foto.titulo">
+      </li> -->
+
+      <!-- faz a iteracao (loop) entre as fotos e retorna uma id da foto-->
+      <li v-for='(foto, i) of fotos' v-bind:key ='i'> 
+        <img v-bind:src="foto.url" v-bind:alt="foto.titulo">
+      </li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+
+    
   </div>
+  
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  data(){
+    return{
+      titulo : "Veterinaria",
+      fotos : [
+        {
+          url : 'https://matsudapet.com.br/blog/wp-content/uploads/2019/08/shutterstock_559799125-compressed.jpg',
+          titulo : 'cachorro'
+        },
+        {
+          url : 'https://matsudapet.com.br/blog/wp-content/uploads/2019/08/shutterstock_559799125-compressed.jpg',
+          titulo : 'cachorro2'
+        }
+      ]
+
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
