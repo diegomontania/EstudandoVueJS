45be89d3ca63b829049f7f1b93017be41f62a54d
diego montania <diegoasmad1@gmail.com>
2020-11-24 19:00:35 -0300

    adicionando projeto de 'api' de onde serão feitas as requisições das imagens

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
 
     <!-- lista de imagens -->
-    <ul>
+    <ul class="lista-fotos">
 
       <!-- faz a iteracao (loop) entre as fotos-->
       <!-- <li v-for="foto of fotos"> 
@@ -11,7 +11,7 @@
       </li> -->
 
       <!-- faz a iteracao (loop) entre as fotos e retorna uma id da foto-->
-      <li v-for='(foto, i) of fotos' v-bind:key ='i'> 
+      <li class="lista-fotos-item" v-for='(foto, i) of fotos' v-bind:key ='i'> 
         <img v-bind:src="foto.url" v-bind:alt="foto.titulo">
       </li>
     </ul>
@@ -25,23 +25,40 @@
 export default {
   data(){
     return{
-      titulo : "Veterinaria",
-      fotos : [
-        {
-          url : 'https://matsudapet.com.br/blog/wp-content/uploads/2019/08/shutterstock_559799125-compressed.jpg',
-          titulo : 'cachorro'
-        },
-        {
-          url : 'https://matsudapet.com.br/blog/wp-content/uploads/2019/08/shutterstock_559799125-compressed.jpg',
-          titulo : 'cachorro2'
-        }
-      ]
-
+      titulo : "Aplicação Vue JS",
+      fotos : []
     }
+  },
+
+  // funcao que é chamada assim que o componente é criado
+  // sobre 'lifecycle-hooks' https://vuejs.org/v2/api/#Options-Lifecycle-Hooks
+  created(){
+    let promise = this.$http.get('http://localhost:3000/v1/fotos'); //promessa de dados
+    promise
+    .then(resposta => resposta.json()) //quando os dados estiverem disponiveis, retorne no formato json
+    .then(fotos => this.fotos = fotos, erro => console.log(err)); //do resultado das fotos, pegue as fotos de fato
   }
+
 }
 </script>
 
 <style>
+  .corpo{
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
 
+  .centralizado{
+    text-align: center;
+  }
+
+  .lista-fotos{
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item{
+    display: inline-block;
+  }
+  
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+import VueResource from 'vue-resource';
+
+// utilizando vue resource após importação
+Vue.use(VueResource);
 
 new Vue({
   el: '#app',
