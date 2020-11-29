62333158d961836cdabda515cbdfc7e1212d2f7a
diego montania <diegoasmad1@gmail.com>
2020-11-29 18:38:56 -0300

    criando componente 'painel'

--- src/App.vue
+++ src/App.vue
@@ -4,42 +4,45 @@
 
     <!-- lista de imagens -->
     <ul class="lista-fotos">
-
-      <!-- faz a iteracao (loop) entre as fotos-->
-      <!-- <li v-for="foto of fotos"> 
-        <img v-bind:src="foto.url" v-bind:alt="foto.titulo">
-      </li> -->
-
       <!-- faz a iteracao (loop) entre as fotos e retorna uma id da foto-->
-      <li class="lista-fotos-item" v-for='(foto, i) of fotos' v-bind:key ='i'> 
-        <img v-bind:src="foto.url" v-bind:alt="foto.titulo">
+      <li class="lista-fotos-item" v-for="foto of fotos" :key="foto"> 
+          <meu-painel :titulo="foto.titulo">
+            <img class="imagem-resposiva" :src="foto.url" :alt="foto.titulo">
+          </meu-painel> 
       </li>
     </ul>
 
-    
   </div>
-  
 </template>
 
 <script>
-export default {
-  data(){
-    return{
-      titulo : "Aplicação Vue JS",
-      fotos : []
+
+  // importando o painel criado
+  import Painel from './components/shared/painel/Painel.vue';
+
+  export default {
+
+    // cria uma chave de referencia para o componente criado, para poder utiliza-lo
+    components : {
+      'meu-painel': Painel
+    },
+
+    data(){
+      return{
+        titulo : "Aplicação Vue JS",
+        fotos : []
+      }
+    },
+
+    // funcao que é chamada assim que o componente é criado
+    // sobre 'lifecycle-hooks' https://vuejs.org/v2/api/#Options-Lifecycle-Hooks
+    created(){
+      let promise = this.$http.get('http://localhost:3000/v1/fotos'); //promessa de dados
+      promise
+        .then(resposta => resposta.json()) //quando os dados estiverem disponiveis, retorne no formato json
+        .then(fotos => this.fotos = fotos, erro => console.log(err)); //do resultado das fotos, pegue as fotos de fato
     }
-  },
-
-  // funcao que é chamada assim que o componente é criado
-  // sobre 'lifecycle-hooks' https://vuejs.org/v2/api/#Options-Lifecycle-Hooks
-  created(){
-    let promise = this.$http.get('http://localhost:3000/v1/fotos'); //promessa de dados
-    promise
-    .then(resposta => resposta.json()) //quando os dados estiverem disponiveis, retorne no formato json
-    .then(fotos => this.fotos = fotos, erro => console.log(err)); //do resultado das fotos, pegue as fotos de fato
   }
-
-}
 </script>
 
 <style>
@@ -61,4 +64,8 @@ export default {
     display: inline-block;
   }
   
+  /* ajustando imagens dentro do painel */
+  .imagem-resposiva{
+    width: 100%;
+  }
 </style>
